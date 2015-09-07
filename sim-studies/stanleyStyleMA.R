fd51e5f474eaeb0e1e9628746f2cfae84d75a08c
evanccarter <evan.c.carter@gmail.com>
2015-09-07 10:32:49 -0500

    made more consistent with Stanley's approach
    1. n1 and n2 were defined as Ni/2. This was wrong, since Ni is the per group size. 
    2. modified formula for variance of d. It now uses the formula other functions we have use/what Stanley uses. 
    3. selection in biased samples no longer based on positive d and p < .05. Now based on positive d and whether zero is included in the CI around d, where variance is calculated without the d^2 term. Stanley uses this because that term will be zero under the null. 
    4. also started outputting t-values and sample sizes so p-curve can be applied

--- sim-studies/stanleyStyleMA.R
+++ sim-studies/stanleyStyleMA.R
@@ -4,8 +4,8 @@ stanleyMA = function(N,eff,het,k,biasProp){
   kBias = floor(k*biasProp)
   kUnbias = k - kBias
   
-  dUnbias = vUnbias = pUnbias = rep(NA,kUnbias)
-  dBias = vBias = pBias = rep(NA,kBias)
+  dUnbias = vUnbias = pUnbias = tUnbias = nUnbias = rep(NA,kUnbias)
+  dBias = vBias = pBias = tBias = nBias = rep(NA,kBias)
   
   for(i in 1:kUnbias){
     
@@ -24,15 +24,19 @@ stanleyMA = function(N,eff,het,k,biasProp){
     v1 = var(Ye)
     m2 = mean(Yc)
     v2 = var(Yc)
-    n1 = Ni/2
-    n2 = Ni/2
+    n1 = Ni
+    n2 = Ni
+    df = 2*Ni-2
     
-    S = sqrt( ((n1 - 1)*v1 + (n2 - 1)*v2)/(n1 + n2 - 2) )
+    S = sqrt( ((n1 - 1)*v1 + (n2 - 1)*v2) / df )
+    
+    test = t.test(Ye,Yc)
     
     dUnbias[i] = (m1 - m2)/S
-    vUnbias[i] = (n1 + n2)/(n1 * n2) + (dUnbias[i]^2)/(2 * (n1 + n2))
-    pUnbias[i] = t.test(Ye,Yc)$p.value
-    
+    vUnbias[i] = (n1 + n2)/(n1 * n2) + (dUnbias[i]^2 / (2 *df)) * (n1 + n2) / df
+    pUnbias[i] = test$p.value
+    tUnbias[i] = as.numeric(test$statistic)
+    nUnbias[i] = 2*Ni
   }
   
   if (biasProp>0){
@@ -52,33 +56,47 @@ stanleyMA = function(N,eff,het,k,biasProp){
         Ye=Te+X3e+50*rnorm(Ni,0,1)
         Yc=X3+50*rnorm(Ni,0,1)
         
-        pObs = t.test(Ye,Yc)$p.value
+        test = t.test(Ye,Yc)
+        pObs = test$p.value
+        tObs = as.numeric(test$statistic)
         
         m1 = mean(Ye)
         v1 = var(Ye)
         m2 = mean(Yc)
         v2 = var(Yc)
-        n1 = Ni/2
-        n2 = Ni/2
+        n1 = Ni
+        n2 = Ni
+        df = 2*Ni-2
         
-        S = sqrt( ((n1 - 1)*v1 + (n2 - 1)*v2)/(n1 + n2 - 2) )
+        S = sqrt( ((n1 - 1)*v1 + (n2 - 1)*v2) / df )
         
         dObs = (m1 - m2)/S
-        vObs = (n1 + n2)/(n1 * n2) + (dUnbias[i]^2)/(2 * (n1 + n2))  
+        vObs = (n1 + n2)/(n1 * n2) + (dObs^2 / (2 *df)) * (n1 + n2) / df 
         
-        if (dObs > 0 && pObs < .05){posSig=1}
+        #test null of d being zero or less (uses different variance)
+        #does so by checking if LL on ci around d is > 0
+        v = (n1 + n2)/(n1 * n2) * (n1 + n2) / df 
+        posSig = if ((dObs - 1.96*sqrt(v)) > 0){1}else{0}
+        
       }
+      
       dBias[i] = dObs
       vBias[i] = vObs
       pBias[i] = pObs
+      tBias[i] = tObs
+      nBias[i] = 2*Ni
+      
     }
   }
   
   #put the unbiased and biased data together for MA j
   d = c(dUnbias,dBias)
-  v = c(vUnbias,vBias)
-  p = c(pUnbias,pBias)
+  v = c(vUnbias,vBias) 
+  p = c(pUnbias,pBias) #p-value from t test
+  t = c(tUnbias,tBias) #t value from t test
+  N = c(nUnbias,nBias) #total sample size
   
-  return(cbind(d,v,p))
+  return(cbind(d,v,p,t,N))
   
 }
+
