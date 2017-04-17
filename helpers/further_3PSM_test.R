f3611d23eef5444e9c635644999b6110a312aa95
Joe Hilgard <jhilgard@asc.upenn.edu>
2017-04-17 16:18:40 -0400

    Another helper file showing how 3PSM breaks down under selprop = 1.

---
+++ helpers/further_3PSM_test.R
@@ -0,0 +1,141 @@
+# Under what conditions does 3PSM fail to return 95% CI?
+# When 3PSM fails to return 95% CI, does it return a point estimate?
+# This is all rather cycle-intensive
+source("start.R")
+
+# What does 3PSM do when 100% are significant?
+set.seed(42069)
+trial1 <- dataMA(k = 10, delta = 0, tau = 0,
+                 empN = T, selProp = 1, qrpEnv = "none") %>% 
+  as.data.frame()
+
+with(trial1, RMA.est(d = d, v = v, long = T))
+with(trial1, TPSM.est(t = t, n1 = n1, n2 = n2, long = T))
+estimate.onestep.selection.heterogeneous(z.obs=trial1$t, n1=trial1$n1, n2=trial1$n2, 
+                                         alpha=0.05/2, 
+                                         theta.init=c(expected.d = 0.3, max.tau= 0.5, p.report = 0.99))
+# returns point estimates but no variance/covariance matrix, not sure what output $ll is
+
+set.seed(777)
+trial2 <- dataMA(k = 10, delta = 0, tau = 0,
+                 empN = T, selProp = 1, qrpEnv = "none") %>% 
+  as.data.frame()
+
+with(trial2, RMA.est(d = d, v = v, long = T))
+with(trial2, TPSM.est(t = t, n1 = n1, n2 = n2, long = T))
+estimate.onestep.selection.heterogeneous(z.obs=trial2$t, n1=trial2$n1, n2=trial2$n2, 
+                                         alpha=0.05/2, 
+                                         theta.init=c(expected.d = 0.3, max.tau= 0.5, p.report = 0.99))
+
+# is this a case in which it might be biased upwards?
+simLength <-  250
+
+output <- NULL
+for (i in 1:simLength) {
+  # Make data
+  trial <- dataMA(k = 10, delta = 0, tau = 0,
+                  empN = T, selProp = 1, qrpEnv = "none") %>% 
+    as.data.frame()
+  
+  # extract estimates
+  rma_out <- with(trial, RMA.est(d = d, v = v, long = T)) %>% 
+    filter(method == "reMA", term == "b0", variable == "estimate")
+  tpsm_out <- with(trial, TPSM.est(t = t, n1 = n1, n2 = n2, long = T)) %>% 
+    filter(method == "3PSM", term == "b0", variable == "estimate")
+  # Tag with run number
+  rma_out$trial <- i
+  tpsm_out$trial <- i
+  
+  # Append to output
+  output <- bind_rows(output, rma_out, tpsm_out)
+}
+
+out.wide <- spread(output, key = method, value = value)
+
+ggplot(out.wide, aes(x = reMA, y = `3PSM`)) +
+  geom_point() +
+  scale_y_continuous(limits = c(0, 1)) +
+  geom_hline(yintercept = 0)
+
+ggplot(output, aes(x = method, y = value)) +
+  geom_boxplot() +
+  scale_y_continuous(limits = c(0, 1)) +
+  geom_hline(yintercept = 0) +
+  coord_flip() +
+  ggtitle("delta = 0, selProp = 100%, tau = 0, k = 10, no QRPs")
+
+# What about with QRPs? Still biased upwards? ----
+simLength <-  100 # very time-consuming
+
+output <- NULL
+for (i in 1:simLength) {
+  # Make data
+  trial <- dataMA(k = 10, delta = 0, tau = 0,
+                  maxN = 500, 
+                  empN = T, selProp = 1, qrpEnv = "high") %>% 
+    as.data.frame()
+  
+  # extract estimates
+  rma_out <- with(trial, RMA.est(d = d, v = v, long = T)) %>% 
+    filter(method == "reMA", term == "b0", variable == "estimate")
+  tpsm_out <- with(trial, TPSM.est(t = t, n1 = n1, n2 = n2, long = T)) %>% 
+    filter(method == "3PSM", term == "b0", variable == "estimate")
+  # Tag with run number
+  rma_out$trial <- i
+  tpsm_out$trial <- i
+  
+  # Append to output
+  output <- bind_rows(output, rma_out, tpsm_out)
+}
+
+out.wide <- spread(output, key = method, value = value)
+
+ggplot(out.wide, aes(x = reMA, y = `3PSM`)) +
+  geom_point() +
+  scale_y_continuous(limits = c(0, 1)) +
+  geom_hline(yintercept = 0)
+
+ggplot(output, aes(x = method, y = value)) +
+  geom_boxplot() +
+  scale_y_continuous(limits = c(0, 1)) +
+  geom_hline(yintercept = 0) +
+  coord_flip() +
+  ggtitle("delta = 0, selProp = 100%, tau = 0, k = 10, high QRPs")
+
+
+# Does increased k fix this when selProp = 1? No. ----
+simLength <-  250
+
+output <- NULL
+for (i in 1:simLength) {
+  # Make data
+  trial <- dataMA(k = 60, delta = 0, tau = 0,
+                  empN = T, selProp = 1, qrpEnv = "none") %>% 
+    as.data.frame()
+  
+  # extract estimates
+  rma_out <- with(trial, RMA.est(d = d, v = v, long = T)) %>% 
+    filter(method == "reMA", term == "b0", variable == "estimate")
+  tpsm_out <- with(trial, TPSM.est(t = t, n1 = n1, n2 = n2, long = T)) %>% 
+    filter(method == "3PSM", term == "b0", variable == "estimate")
+  # Tag with run number
+  rma_out$trial <- i
+  tpsm_out$trial <- i
+  
+  # Append to output
+  output <- bind_rows(output, rma_out, tpsm_out)
+}
+
+out.wide <- spread(output, key = method, value = value)
+
+ggplot(out.wide, aes(x = reMA, y = `3PSM`)) +
+  geom_point() +
+  scale_y_continuous(limits = c(0, 1)) +
+  geom_hline(yintercept = 0)
+
+ggplot(output, aes(x = method, y = value)) +
+  geom_boxplot() +
+  scale_y_continuous(limits = c(0, 1)) +
+  geom_hline(yintercept = 0) +
+  coord_flip() +
+  ggtitle("delta = 0, selProp = 100%, tau = 0, k = 10, no QRPs")
